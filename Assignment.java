ac01580d3f167ec5f2f40c0b7ef7bf2a4379f668
Udari Rathnasiri <udarihashini@gmail.com>
2023-08-08 01:20:59 +0530

    Completed Assignment-03

--- Assignment.java
+++ Assignment.java
@@ -26,6 +26,7 @@ public class Assignment{
             int age = scanner.nextInt();
 
             if ((age >= 10) && (age <= 18)) {
+                
                 scanner.nextLine();
                 correctAge = age;
                 System.out.print("Enter Your Subject1: ");
